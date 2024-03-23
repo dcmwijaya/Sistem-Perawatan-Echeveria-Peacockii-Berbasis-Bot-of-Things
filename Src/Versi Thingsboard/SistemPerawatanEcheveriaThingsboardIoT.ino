32160da86d3c86ac299d9d833d44bc07a87da061
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-23 10:58:08 +0700

    Update SistemPerawatanEcheveriaThingsboardIoT.ino

--- Src/Versi Thingsboard/SistemPerawatanEcheveriaThingsboardIoT.ino
+++ Src/Versi Thingsboard/SistemPerawatanEcheveriaThingsboardIoT.ino
@@ -54,18 +54,18 @@ String sendMsg, msg1, msg2;
 #define ON "ON"
 #define OFF "OFF"
 bool relayON = HIGH; bool relayOFF = LOW;
+int kontrolair;
 
 // Variabel untuk keperluan sensor
-const float GAMMA = 0.7, RL10 = 50;
-int kontrolair, analogLDR, kelembapan_udara, kelembapan_tanah;
-float volt, resistance, suhu_udara, cahaya;
-String statusUdara, statusTanah, statusSinar, info_suhuudara, info_kelembapanudara, info_kelembapantanah, info_intensitascahaya;
+const float GAMMA = 0.7, RL10 = 50; int analogLDR; float volt, resistance, cahaya; String statusSinar, info_intensitascahaya; // LDR
+int kelembapan_udara; float suhu_udara; String statusUdara, info_suhuudara, info_kelembapanudara; // DHT
+int kelembapan_tanah; String statusTanah, info_kelembapantanah; // FC-28
 
 // Method untuk mengatur konektivitas
 void ConnectToWiFi() {
   WiFi.mode(WIFI_STA); // Membuat perangkat sebagai station
   WiFi.begin(WIFISSID, PASSWORD); Serial.print("Menyambungkan ke jaringan"); // Memulai jaringan
-  while (WiFi.status() != WL_CONNECTED) { // Jika tidak berhasil terhubung ke jaringan maka cetak di serial monitor :
+  while (WiFi.status() != WL_CONNECTED) { // Selama tidak berhasil terhubung ke jaringan maka cetak di serial monitor :
     Serial.print("."); delay(500);
   }
   if (WiFi.status() == WL_CONNECTED) { // Jika berhasil terhubung ke jaringan maka cetak di serial monitor :
@@ -73,25 +73,27 @@ void ConnectToWiFi() {
   }
 }
 
-
-void reconnect() { //Proses menyambungkan ulang ke WiFi
-  while (!client.connected()) {
-    status = WiFi.status();
-    if ( status != WL_CONNECTED) {
-      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(500); Serial.print(".");
+// Method untuk menyambungkan ulang ke WiFi
+void reconnect() { 
+  while (!client.connected()) { // Selama client tidak terhubung ke jaringan maka :
+    status = WiFi.status(); // Status WiFi
+    if (status != WL_CONNECTED) { // Jika tidak berhasil terhubung ke jaringan maka cetak di serial monitor :
+      WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // Memulai jaringan
+      while (WiFi.status() != WL_CONNECTED) { // Selama tidak berhasil terhubung ke jaringan maka cetak di serial monitor :
+        Serial.print("."); delay(500);
       }
-      Serial.println("\nBerhasil tersambung ke Access Point..");
+      if (WiFi.status() == WL_CONNECTED) { // Jika berhasil terhubung ke jaringan maka cetak di serial monitor :
+        Serial.println("\nTelah terhubung ke "+String(WIFISSID)+"\n\n");
+      }
     }
-    if (client.connect(DEVICE_ID_TB, ACCESS_TOKEN_TB, "")) {
+    if (client.connect(DEVICE_ID_TB, ACCESS_TOKEN_TB, "")) { // Jika berhasil terhubung ke ThingsBoard maka cetak di serial monitor :
       Serial.println("Menyambungkan ke Node ThingsBoard ...[SUKSES]\n");
     } 
   }
 }
 
-
-void connectBot() { //Memulai menyambungkan ke Bot Telegram
+// Method untuk menyambungkan ke Bot Telegram
+void connectBot() {
   myBot.setTelegramToken(BOTtoken);
   myBot.wifiConnect(WIFI_SSID, WIFI_PASSWORD); 
   myBot.setMaxConnectionRetries(5);
@@ -108,63 +110,70 @@ void connectBot() { //Memulai menyambungkan ke Bot Telegram
   }
 }
 
-
-void ButtonBot() { //Bagian yang mengatur visualisasi tombol di bot telegram
-  //monitoring menu dalam bentuk inline button
+// Method untuk mengatur visualisasi tombol bot telegram
+void ButtonBot() { 
+  // Monitoring menu dalam bentuk inline button
   InlineKey.addButton("🌤️ Monitoring Temperature", InlineMenu1, CTBotKeyboardButtonQuery);
-  InlineKey.addRow(); //buat baris baru atau enter
+  InlineKey.addRow();
   InlineKey.addButton("🌦️ Monitoring Humidity", InlineMenu2, CTBotKeyboardButtonQuery);
-  InlineKey.addRow(); //buat baris baru atau enter
+  InlineKey.addRow();
   InlineKey.addButton("🌱 Monitoring Soil Moisture", InlineMenu3, CTBotKeyboardButtonQuery);
-  InlineKey.addRow(); //buat baris baru atau enter
+  InlineKey.addRow();
   InlineKey.addButton("☀️ Monitoring Light Intensity", InlineMenu4, CTBotKeyboardButtonQuery);
-  InlineKey.addRow(); //buat baris baru atau enter
+  InlineKey.addRow();
   InlineKey.addButton("🚰 Controlling Water Pump", InlineMenu5, CTBotKeyboardButtonQuery);
-  InlineKey.addRow(); //buat baris baru atau enter
-  //controlling menu dalam bentuk inline button
+  InlineKey.addRow();
+  
+  // Menu kontrol dalam bentuk inline button
   InlineOption.addButton("✅ Pump: Turn ON", ON, CTBotKeyboardButtonQuery);
   InlineOption.addButton("❌ Pump: Turn OFF", OFF, CTBotKeyboardButtonQuery);
-  viewTombol = false; //Tombol -> default : hidden
+  
+  // Tombol -> default : hidden
+  viewTombol = false;
 }
 
-
-void KirimTB(){ //mengirim data ke Thingsboard
+// Method untuk mengirim data ke Thingsboard
+void KirimTB(){
   serializeJson(DataJSON, Payload);
   client.publish("v1/devices/me/telemetry/fpiotdevan", Payload);
   client.publish("v1/devices/me/attributes/fpiotdevan", Payload);
   Serial.println(String("Mengirimkan data sebagai berikut:\n")+Payload+String(" ...[SUKSES]\n"));
 }
 
-
-void BacaSensor(){ //Pembacaan data pada sensor
-  //baca nilai sensor DHT22
+// Method untuk membaca sensor
+void BacaSensor(){
+  // Baca nilai sensor DHT22
   suhu_udara = dht.readTemperature();
   kelembapan_udara = dht.readHumidity();
-  //baca nilai sensor FC-28
+  
+  // Baca nilai sensor FC-28
   kelembapan_tanah = fc28.getSoilMoisture();
-  //baca nilai sensor LDR
+  
+  // Baca nilai sensor LDR
   analogLDR = analogRead(LDR_PIN);
   volt = analogLDR * 5 / 4095.0;
   resistance = 2000 * volt / (1 - volt / 5);
   cahaya = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
-  //kirim data ke Thingsboard dengan protokol MQTT
+  
+  // Kirim data ke Thingsboard menggunakan protokol MQTT
   DataJSON["Suhu Udara"] = String(suhu_udara);
   DataJSON["Kelembapan Udara"] = String(kelembapan_udara);
   DataJSON["Kelembapan Tanah"] = String(kelembapan_tanah);
   DataJSON["Intensitas Cahaya"] = String(cahaya);
+  
   TresholdSensorState(); //memanggil method ThresholdSensorState
   PrintLCD(); //memanggil method PrintLCD
   KirimTB(); //memanggil method KirimTB
 }
 
-
-void TresholdSensorState(){ //batasan suhu, kelembapan, dan intensitas cahaya
+// Method untuk menentukan batasan suhu, kelembapan, dan intensitas cahaya
+void TresholdSensorState(){
   if (suhu_udara >= 0 && suhu_udara < 16) {
     if (kelembapan_udara > 90 && kelembapan_udara <=100) {
       if (cahaya >= 500) {  
-        info_suhuudara = "Suhu Udara: Rendah";               //Dingin
-        info_kelembapanudara = "Kelembapan Udara: Tinggi";   //Basah
-        info_intensitascahaya = "Intensitas Cahaya: Rendah"; //Gelap
+        info_suhuudara = "Suhu Udara: Rendah";               // Dingin
+        info_kelembapanudara = "Kelembapan Udara: Tinggi";   // Basah
+        info_intensitascahaya = "Intensitas Cahaya: Rendah"; // Gelap
         statusUdara = "Status Kualitas Udara: Bahaya";
         statusSinar = "Status Kualitas Sinar: Aman";
         digitalWrite(RPOMPA1_PIN, relayOFF);
@@ -175,9 +184,9 @@ void TresholdSensorState(){ //batasan suhu, kelembapan, dan intensitas cahaya
   if (suhu_udara >= 16 && suhu_udara <= 34) {
     if (kelembapan_udara >= 30 && kelembapan_udara <= 90) {
       if (cahaya >= 200 && cahaya < 500) {  
-        info_suhuudara = "Suhu Udara: Normal";               //Remang-remang
-        info_kelembapanudara = "Kelembapan Udara: Normal";   //Lembap
-        info_intensitascahaya = "Intensitas Cahaya: Normal"; //Remang-remang
+        info_suhuudara = "Suhu Udara: Normal";               // Remang-remang
+        info_kelembapanudara = "Kelembapan Udara: Normal";   // Lembap
+        info_intensitascahaya = "Intensitas Cahaya: Normal"; // Remang-remang
         statusUdara = "Status Kualitas Udara: Aman";
         statusSinar = "Status Kualitas Sinar: Aman";
         digitalWrite(RPOMPA1_PIN, relayOFF);
@@ -188,9 +197,9 @@ void TresholdSensorState(){ //batasan suhu, kelembapan, dan intensitas cahaya
   if (suhu_udara > 34 && suhu_udara <= 100) {
     if (kelembapan_udara >= 0 && kelembapan_udara < 30) {
       if (cahaya < 200) {
-        info_suhuudara = "Suhu Udara: Tinggi";                 //Panas
-        info_kelembapanudara = "Kelembapan Udara: Rendah";     //Kering
-        info_intensitascahaya = "Intensitas Cahaya: Tinggi";   //Cerah
+        info_suhuudara = "Suhu Udara: Tinggi";                 // Panas
+        info_kelembapanudara = "Kelembapan Udara: Rendah";     // Kering
+        info_intensitascahaya = "Intensitas Cahaya: Tinggi";   // Cerah
         statusUdara = "Status Kualitas Udara: Bahaya";
         statusSinar = "Status Kualitas Sinar: Bahaya";
         digitalWrite(RPOMPA1_PIN, relayON);
@@ -199,26 +208,26 @@ void TresholdSensorState(){ //batasan suhu, kelembapan, dan intensitas cahaya
     }
   } 
   if (kelembapan_tanah >= 60){
-    info_kelembapantanah = "Kelembapan Tanah: Tinggi";      //Basah
+    info_kelembapantanah = "Kelembapan Tanah: Tinggi";      // Basah
     statusTanah = "Status Kualitas Tanah: Bahaya";
     digitalWrite(RPOMPA1_PIN, relayOFF);
     DataJSON["Pompa 1"] = "0"; //nilai OFF = 0
   }
   if (kelembapan_tanah > 40 && kelembapan_tanah < 60) { 
-    info_kelembapantanah = "Kelembapan Tanah: Normal";      //Lembap
+    info_kelembapantanah = "Kelembapan Tanah: Normal";      // Lembap
     statusTanah = "Status Kualitas Tanah: Aman";
     digitalWrite(RPOMPA1_PIN, relayOFF);
     DataJSON["Pompa 1"] = "0"; //nilai OFF = 0
   }
   if (kelembapan_tanah <= 40) {
-    info_kelembapantanah = "Kelembapan Tanah: Rendah";     //Kering
+    info_kelembapantanah = "Kelembapan Tanah: Rendah";     // Kering
     statusTanah = "Status Kualitas Tanah: Bahaya";
     digitalWrite(RPOMPA1_PIN, relayON);
     DataJSON["Pompa 1"] = "1"; //nilai ON = 1
   }
 }
 
-
+// Method untuk mengatur bot telegram
 void botTelegram() {
   TBMessage msg; //Constructor TBMessage
   
@@ -310,8 +319,8 @@ void botTelegram() {
   }
 }
 
-
-void LCDinit(){ //Memulai komunikasi serial dengan LCD
+// Method untuk memulai LCD
+void LCDinit(){
   lcd.init();
   //start LCD
   lcd.clear(); lcd.backlight(); lcd.setCursor(1,0); lcd.print("Starting"); lcd.setCursor(1,1); lcd.print("Smart System..."); delay(2500);
@@ -321,28 +330,28 @@ void LCDinit(){ //Memulai komunikasi serial dengan LCD
   lcd.clear(); lcd.backlight(); lcd.setCursor(1,0); lcd.print("Loading...."); delay(5000);
 }
 
-
-void PrintLCD(){ //Menampilkan data ke LCD
+// Method untuk menampilkan data ke LCD
+void PrintLCD(){
   lcd.clear(); lcd.backlight(); lcd.setCursor(1,0); lcd.print("Suhu Udara:"); lcd.setCursor(1,1); lcd.print(""+String(suhu_udara)+" "+String((char)223)+"C"); delay(1000);
   lcd.clear(); lcd.backlight(); lcd.setCursor(1,0); lcd.print("Kelem.Udara:"); lcd.setCursor(1,1); lcd.print(""+String(kelembapan_udara)+" %"); delay(1000);
   lcd.clear(); lcd.backlight(); lcd.setCursor(1,0); lcd.print("Kelem.Tanah:"); lcd.setCursor(1,1); lcd.print(""+String(kelembapan_tanah)+" %"); delay(1000);
   lcd.clear(); lcd.backlight(); lcd.setCursor(1,0); lcd.print("Int.Cahaya:"); lcd.setCursor(1,1); lcd.print(""+String(cahaya)+" lx"); delay(1000);
 }
 
-
-void setup() { //Method yang dijalankan sekali
+// Method yang dijalankan sekali
+void setup() {
   //inisialisasi koneksi dan komponen
   Serial.begin(SERIAL_DEBUG_BAUD);
   ConnectToWiFi();
   client.setServer(MQTT_SERVER, 1883);
   ButtonBot();
-  fc28.initFC28Sensor(SERIAL_DEBUG_BAUD, FC28_PIN);
-  dht.begin();
+  fc28.begin(); // Memulai sensor fc-28
+  dht.begin(); // Memulai sensor dht
   LCDinit();
   connectBot();
+  
   //Inisialisasi gpio
   pinMode(LDR_PIN, INPUT);
-  pinMode(FC28_PIN, INPUT);
   pinMode(DHT_PIN, INPUT);
   pinMode(RPOMPA1_PIN, OUTPUT);
   pinMode(RPOMPA2_PIN, OUTPUT);
